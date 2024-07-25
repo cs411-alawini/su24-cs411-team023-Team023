09c2a4b1d713b3734fd83d7e97fb15c6b1b814a3
Nynika Badam <nbadam2@illinois.edu>
2024-07-25 17:34:53 -0500

    launch locally

---
+++ NutriChoice/index.cjs
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const db = mysql.createConnection({
+  host: '34.134.234.150',
+  user: 'root',
+  password: 'team1234',
+  database: 'NutriChoice'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/search', (req, res) => {
+  const { query } = req.query;
+  const sqlQuery = 'SELECT * FROM VitMacroData WHERE shrt_desc LIKE ?';
+  db.query(sqlQuery, [`%${query}%`], (err, results) => {
+    if (err) {
+      res.status(500).send('Database query error');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
